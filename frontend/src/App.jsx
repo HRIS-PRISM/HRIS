bf36d9eb118f90b42659d951973ffdcbf27df564
MoralesTA <morales.ta.bsinfotech@gmail.com>
2026-02-25 17:00:36 +0800

    Attendance/HomeUser/App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1406,7 +1406,7 @@ function App() {
   path="/leave-request-user"
   element={
     <ProtectedRoute
-      allowedRoles={["administrator", "superadmin", "technical"]}
+      allowedRoles={["administrator", "superadmin", "technical", "staff"]}
     >
       <LeaveRequestUser />
     </ProtectedRoute>
