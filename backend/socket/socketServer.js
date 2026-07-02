a1cc12c6b8fb581601d3c4fb467eaa3e586c29cc
hanapot0324 <hannasarabia879@gmail.com>
2026-07-02 14:13:20 +0800

    pulled

--- backend/socket/socketServer.js
+++ backend/socket/socketServer.js
@@ -14,7 +14,7 @@ function initializeSocket(server) {
     'http://192.168.50.36:5137',
     'http://192.168.50.45:5137',
     'http://136.239.248.42:5137',
-    'http://192.168.50.97:5137',
+    'http://192.168.50.49:5137',
   ];
 
   function isOriginAllowed(origin) {
